c9828632f4e3bf3823f0daa7ef072bca2cf9f81b
soyuka <soyuka@gmail.com>
2018-03-22 20:31:40 +0100

    learn more

--- learn/learn.rs
+++ learn/learn.rs
@@ -56,6 +56,66 @@ fn main() {
         println!("{}!", number);
     }
 
+    // test with a struct
+    // expression, defines a new scope, trying understand ownership
+    // let mut e = 0;
+    //
+    // {
+    //     e = 1; //x is 500 from tuple
+    // };
+    //
+    // println!("Test {}", e);
+
+    let my_str = "First Word of this string";
+
+    println!("First Word of this string: {}", first_word(my_str));
+
+    let mut poney = gimme_a_poney(String::from("PONEY"), String::from("PINK"));
+
+    println!("My poney name is {} and its color is {}", poney.name, poney.color);
+    poney.color = String::from("MULTI"); // coz mut
+    poney.disable();
+
+    println!("Show poney (needs debug annotation) {:?}", poney);
+
+    let poney_buddy = Poney::new_poney(String::from("PONEY"), String::from("PINK"));
+
+}
+
+#[derive(Debug)]
+struct Poney {
+    name: String,
+    color: String,
+    active: bool
+}
+
+impl Poney {
+    fn disable(&mut self) {
+        self.active = !self.active;
+    }
+
+    fn new_poney(name: String, color: String) -> Poney {
+        Poney { name, color, active: true }
+    }
+}
+
+fn gimme_a_poney(name: String, color: String) -> Poney {
+    Poney {
+        name,
+        color,
+        active: true
+    }
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i]
+        }
+    }
+
+    &s[..]
 }
 
 // use snake
